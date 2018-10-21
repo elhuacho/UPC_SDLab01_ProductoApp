e3cd31f2dd4b3e27af48f0dc74f9be4d0ff197cd
Walter <U201620367@upc.edu.pe>
2018-10-20 23:36:04 -0500

    Se agrego entidad Producto

--- src/app/com/entidades/Producto.java
+++ src/app/com/entidades/Producto.java
@@ -1,4 +1,31 @@
 package app.com.entidades;
 
 public class Producto {
+    private Long codigo;
+    private String descripcion;
+    private Double precio;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
 }
